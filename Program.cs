e78ed79b536db5fac94632219b88a163c0550bd2
Maria Luiza <malualper@hotmail.com>
2020-06-01 22:27:26 -0300

    horasistema

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace horasistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("data e hora atual do sistema");
+            DateTime horasistema = DateTime.Now;
+            Console.WriteLine($"hora sistema:{horasistema}");
+        }
+    }
+}
